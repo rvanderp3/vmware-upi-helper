b9afe7f47f60e8adc76c188aeae982656132a5c3
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2021-09-22 14:12:31 -0400

    fixed ignition server returning partial ignition

--- platform_none/bootstrap-serv.sh
+++ platform_none/bootstrap-serv.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
-PORT=9999
-FILE=/home/core/bootstrap.ign
-
-if [ ! -f "$FILE" ]; then
-    echo $FILE not found
-    exit 1
-fi
-MIME_TYPE=$(mimetype "$FILE")
-SIZE_BYTES=$(du -b "$FILE" | cut -f1)
-FILE_NAME=$(basename "$FILE")
-
-HEADER="\
-HTTP/1.1 200 OK
-Content-Type: $MIME_TYPE
-Content-Disposition: attachment; filename=$FILE_NAME
-Content-Length: $SIZE_BYTES
-
-"
-
 sudo systemctl enable dnsmasq
 sudo systemctl start dnsmasq
-
-while true; do
-socat -d -d - tcp-l:"$PORT",reuseaddr < <(printf "$HEADER"; cat "$FILE")
-done
+mkdir bootstrap
+cp bootstrap.ign bootstrap
+podman run -v /home/core/bootstrap:/serv:Z -p 9999:9999 --entrypoint python3 registry.access.redhat.com/ubi8/python-39 -m http.server 9999 --directory /serv
